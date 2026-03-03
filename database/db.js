55afb6396f5cc3e240222f04849239c8c5e1b1e5
VolodymyrOsinchuk <osinchuk28@live.com>
2026-03-03 21:33:17 +0100

    updated version package.json

--- database/db.js
+++ database/db.js
@@ -1,19 +1,20 @@
 const mongoose = require("mongoose");
 
-const DB = process.env.DATABASE.replace(
-  "<PASSWORD>",
-  process.env.DATABASE_PASSWORD
-);
+// const DB = process.env.DATABASE.replace(
+//   "<PASSWORD>",
+//   process.env.DATABASE_PASSWORD,
+// );
+
+const DB = process.env.DATABASE;
 
 const connectDB = async () => {
-  await mongoose
-    .connect(DB)
-    .then((con) => {
-      console.log(`Успішне підключення до БД на хості ${con.connection.host}`);
-    })
-    .catch((err) => {
-      console.log("Помилка підключення до БД: " + err.message);
-    });
+  try {
+    const con = await mongoose.connect(DB);
+    console.log(`Успішне підключення до БД на хості ${con.connection.host}`);
+  } catch (err) {
+    console.log("Помилка підключення до БД: " + err.message);
+    process.exit(1); // бажано завершити процес, якщо БД не підключилась
+  }
 };
 
 module.exports = connectDB;
